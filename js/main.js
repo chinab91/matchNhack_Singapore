cc5fe067d056e678452fa7a09ba9be74fba3dffa
Rtexal Tan <r.rtexal@gmail.com>
2013-03-23 23:58:41 +0800

    interface commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+$("#lab1").click( function () {
+  $('#ADD').css("background-color","#1ABC9C");
+  $('.modal-foot').css("background-color","#1ABC9C");
+  $('textarea').css("background-color","#1ABC9C");
+   });
+$("#lab2").click( function () {
+  $('#ADD').css("background-color","#2ECC71");
+  $('.modal-foot').css("background-color","#2ECC71");
+  $('textarea').css("background-color","#2ECC71");
+   });
+$("#lab3").click( function () {
+  $('#ADD').css("background-color","#3498DB");
+  $('.modal-foot').css("background-color","#3498DB");
+  $('textarea').css("background-color","#3498DB");
+   });
+$("#lab4").click( function () {
+  $('#ADD').css("background-color","#9B59B6");
+  $('.modal-foot').css("background-color","#9B59B6");
+  $('textarea').css("background-color","#9B59B6");
+   });
+$("#lab5").click( function () {
+  $('#ADD').css("background-color","#E67E22");
+  $('.modal-foot').css("background-color","#E67E22");
+  $('textarea').css("background-color","#E67E22");
+   });
+$("#lab6").click( function () {
+  $('#ADD').css("background-color","#E74C3C");
+  $('.modal-foot').css("background-color","#E74C3C");
+  $('textarea').css("background-color","#E74C3C");
+   });
 
